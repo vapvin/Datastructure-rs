fddb94bdd13706166a222d0afe8645dbc9370ebf
vapvin <vins@vins.dev>
2020-10-28 05:42:21 +0900

    Linked List Create => Method New, Add

--- src/linked_list.rs
+++ src/linked_list.rs
@@ -16,3 +16,43 @@ impl<T> Node<T>{
         }
     }
 }
+
+pub struct LinkedList<T>{
+    length: u32,
+    start: Option<NonNull<Node<T>>>,
+    end: Option<NonNull<Node<T>>>,
+}
+
+impl<T> Default for LinkedList<T>{
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
+impl<T> LinkedList<T> {
+    pub fn new() -> Self {
+        Self {
+            length: 0,
+            start: None,
+            end: None,
+        }
+    }
+
+    pub fn add(&mut self, obj: T){
+        let mut node = Box::new(Node::new(obj));
+        unsafe {
+            node.next = None;
+            node.prev = self.end;
+
+            let node_ptr = Some(NonNull::new_unchecked(Box::into_raw(node)));
+            match self.end {
+                Node => self.start = node_ptr,
+                Some(end_ptr) => (*end_ptr.as_ptr()).next = node_ptr,
+            }
+
+            self.end = node_ptr;
+        }
+        self.length += 1;
+    }
+    
+}
